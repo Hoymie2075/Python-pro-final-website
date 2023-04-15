b95a74bd211f66087d088f7631c45e8c394cfcb9
Hoymie_Andrey <rytsar0pravdy@gmail.com>
2023-04-15 21:49:16 +0200

    added pagination

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// index.js
+
 import { countries } from "./countries-list.js";
 
 // Get the search input element
 const searchInput = document.getElementById('searchInput');
-console.log(searchInput.value)
 renderCountries(countries)
 // Add an event listener for input event on the search input
 searchInput.addEventListener('input', (event) => {
@@ -17,11 +18,58 @@ searchInput.addEventListener('input', (event) => {
 
 // Function to render countries based on the filtered array
 function renderCountries(countriesArray) {
+    // Clear the existing cards and pagination
+    const container = document.getElementById('country-cards-container');
+    container.innerHTML = '';
+    const pagination = document.getElementById('pagination');
+    pagination.innerHTML = '';
+
+    // Calculate the number of pages required to display all the countries
+    const numPages = Math.ceil(countriesArray.length / 36);
+
+    // Loop through the pages and create a button for each page
+    for (let i = 1; i <= numPages; i++) {
+        // Create a new button element for the page
+        const button = document.createElement('button');
+        button.innerHTML = i;
+
+        // Add an event listener for click event on the page button
+        button.addEventListener('click', () => {
+            // Calculate the start and end index for the countries to be displayed on this page
+            const startIndex = (i - 1) * 36;
+            const endIndex = i * 36;
+
+            // Call a function to render the countries for this page
+            renderCountriesPage(countriesArray.slice(startIndex, endIndex));
+
+            // Remove the active class from all buttons
+            const buttons = document.querySelectorAll('.pagination button');
+            buttons.forEach(button => button.classList.remove('active'));
+
+            // Add the active class to the selected button
+            button.classList.add('active');
+        });
+
+        // Append the button to the pagination element
+        pagination.appendChild(button);
+    }
+
+    // Render the first page of countries by default
+    renderCountriesPage(countriesArray.slice(0, 36));
+
+    // Add the active class to the first button
+    const firstButton = document.querySelector('.pagination button');
+    firstButton.classList.add('active');
+}
+  
+
+// Function to render countries for a specific page
+function renderCountriesPage(countriesArray) {
     // Clear the existing cards
     const container = document.getElementById('country-cards-container');
     container.innerHTML = '';
 
-    // Loop through the filtered countries array and create a card for each country
+    // Loop through the countries array and create a card for each country
     countriesArray.forEach(country => {
         // Create a new div element for the card
         const cardDiv = document.createElement('div');
@@ -39,5 +87,4 @@ function renderCountries(countriesArray) {
 
         // Append the card to the container element
         container.appendChild(cardDiv);
-    });
-}
+    })};
